92eaa7a9e830a61e1771a56ded80b64258bfb0cc
MeganAlexia <megan.arneau@gmail.com>
2020-06-05 12:32:09 +0100

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MockData } from './mock-data';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'glossary';
+  letters = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z'
+  ];
+  data = MockData;
+
+  public createEntry(event: any): void {
+    console.log('create entry', event);
+  }
 }
---
+++ src/app/mock-data.ts
@@ -0,0 +1,12 @@
+export const MockData = [
+  { id: 11, name: 'Dr Nice', description: 'Test' },
+  { id: 12, name: 'Narco', description: 'Test' },
+  { id: 13, name: 'Bombasto', description: 'Test' },
+  { id: 14, name: 'Celeritas', description: 'Test' },
+  { id: 15, name: 'Magneta', description: 'Test' },
+  { id: 16, name: 'RubberMan', description: 'Test' },
+  { id: 17, name: 'Dynama', description: 'Test' },
+  { id: 18,  name: 'Dr IQ', description: 'Test' },
+  { id: 19,  name: 'Magma', description: 'Test' },
+  { id: 20,  name: 'Tornado', description: 'Test' }
+];
